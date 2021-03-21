85810f4324089ab9737222cc51ac60e527b5f7b0
wibaklein <wibaklein@gmail.com>
2021-03-21 21:19:08 +0100

    counter working

--- main.js
+++ main.js
@@ -5,5 +5,17 @@ function updateCounters() {
     const totalCount = document.getElementById("total_count");
     const totalTodos = document.getElementsByClassName("todo_item").length;
     totalCount.innerHTML = " Total "+totalTodos;    
+    
+    //total of done items in summary
+    const completedCount = document.getElementById("completed_count");
+    const totalDone = document.getElementsByClassName("completed").length;
+    completedCount.innerHTML = " Done "+totalDone; 
+    //total of uncompleted items in summary
+    
+    const uncompletedCount = document.getElementById("uncompleted_count");
+    const uncompletedTodos = totalTodos - completedCount;
+    uncompletedCount.innerHTML = " Todo "+ uncompletedTodos;
+   
 }
 updateCounters();
+
