634634fb8adb73eb8d7064d7a29937189380f22f
wibaklein <wibaklein@gmail.com>
2021-03-22 12:52:27 +0100

    add list items working

--- main.js
+++ main.js
@@ -41,8 +41,54 @@ function toggleDone(event) {
 }
   // and use the "toggleDone" function as the callback
 
-  
-  document.querySelector("form").addEventListener("submit",function addNewTodo(event) {
+/// MAANDAG ///// /// MAANDAG ///// /// MAANDAG ///// 
+
+/*   document.querySelector("form").addEventListener("submit",function addNewTodo(event) { // ?? addNEwtodo moet zijn createTodo? 
     event.preventDefault();
-   // console.log("i got clicked");
+   console.log("i got clicked");
+  }); */
+
+
+  function createTodo(title) {
+    // create a label, create a checkbox  
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = false;
+  
+    // add the "change" event listener to the checkbox
+  checkbox.addEventListener("change", toggleDone); // waarom moet dit hier nog een keer? en wwaarom zonder loop?
+    // and append the checkbox to the label
+    label.appendChild(checkbox);   // ? wel en niet tussen haakjes?? why?
+  
+    // create a text node with the given title
+    const labelText = document.createTextNode(" " + title); // ? wat is een textNode? hoe weet ik dat?
+    
+    // and append the text node to the label
+    label.appendChild(labelText); // ?wel en niet tussen haakjes?? why? iig onthouden
+    
+    // create a list item
+    const listItem = document.createElement("li") //? kan ik de list item ook als eerste aanmaken?
+    listItem.className = "todo_item";
+    // add label to list item
+    listItem.appendChild(label);
+    
+    // append the list item to the todo list
+    const list = document.getElementById("todolist");
+    
+    list.appendChild(listItem);
+  }
+  document.querySelector("form").addEventListener("submit", function addNewTodo(event) {
+    event.preventDefault();
+
+    const inputField = document.querySelector("#new-todo");
+    const newTodoTitle = inputField.value;
+    createTodo(newTodoTitle);
+
+    // reset the value of the inputField to make it empty and
+    // ready to create new todos
+    inputField.value = null;
+
+    updateCounters();
   });
+  
